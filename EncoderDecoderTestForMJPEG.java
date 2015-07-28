c1aa015aa2a5f23951ca50a4c857c8b8bc576b79
Alan Wang <alan.wang@csr.com>
2015-07-28 20:45:32 +0800

    Add MJPEG Decoder Test Case

--- EncoderDecoderTestForMJPEG.java
+++ EncoderDecoderTestForMJPEG.java
@@ -39,8 +39,10 @@ public class EncoderDecoderTestForMJPEG extends AndroidTestCase {
     // Parameters for encoder
     private static final String MIME_TYPE = "video/mjpeg";
 
+    private static final int TIME_OUT = 10000; //usec
+
     private static final int FRAME_RATE = 15;               // 15fps
-    private static final int NUM_FRAMES = 1;                // Only one frame
+    private static final int NUM_FRAMES = 10;                // Only one frame
     private static final int IFRAME_INTERVAL = 10;          // 10 secs between I-frames
 
 
@@ -62,6 +64,12 @@ public class EncoderDecoderTestForMJPEG extends AndroidTestCase {
         encodeVideoFromBuffer();
     }
 
+    public void testDecodeVideoFromBufToBuf720p() throws Exception {
+        Log.i(LOG_TAG, "Decode Video from Buffer to Buffer 720p");
+        setParameters(1280, 720, 6000000);
+        decodeVideoFromBuffer();
+    }
+
     /**
      * Sets the desired frame size and bit rate.
      */
@@ -78,7 +86,7 @@ public class EncoderDecoderTestForMJPEG extends AndroidTestCase {
         MediaCodec encoder = null;
 
         try {
-            MediaCodecInfo codecInfo = selectCodec(MIME_TYPE);
+            MediaCodecInfo codecInfo = selectCodec(MIME_TYPE, true);
             if (codecInfo == null) {
                 Log.e(LOG_TAG, "Unable to find an appropriate codec for " + MIME_TYPE);
                 return;
@@ -87,7 +95,7 @@ public class EncoderDecoderTestForMJPEG extends AndroidTestCase {
             Log.i(LOG_TAG, "Found codec: " + codecInfo.getName());
 
             // JPEG encoder only support this colorFormat as Input
-            // COLOR_FormatYCbYCr a.k.a Organized as 16bit YUYV
+            // COLOR_FormatCbYCrY a.k.a Organized as 16bit UYVY
             int colorFormat = MediaCodecInfo.CodecCapabilities.COLOR_FormatCbYCrY;
 
             MediaFormat format = MediaFormat.createVideoFormat(MIME_TYPE, mWidth, mHeight);
@@ -102,19 +110,16 @@ public class EncoderDecoderTestForMJPEG extends AndroidTestCase {
 
             // Create a MediaCodec for the desired codec, then configure it as an encoder with
             // our desired properties.
-            Log.i(LOG_TAG, "creating Codec " + codecInfo.getName());
             encoder = MediaCodec.createByCodecName(codecInfo.getName());
             Log.i(LOG_TAG, "created Codec " + codecInfo.getName());
-            Log.i(LOG_TAG, "configuring codc...");
             encoder.configure(format, null, null, MediaCodec.CONFIGURE_FLAG_ENCODE);
-            Log.i(LOG_TAG, "encoder starting...");
             encoder.start();
 
             doEncodeVideoFromBuffer(encoder, colorFormat);
 
         } finally {
 
-            Log.i(LOG_TAG, "releasing codec");
+            Log.i(LOG_TAG, "releasing encoder");
             if (encoder != null) {
                 encoder.stop();
                 encoder.release();
@@ -124,17 +129,64 @@ public class EncoderDecoderTestForMJPEG extends AndroidTestCase {
 
     }
 
+    private void decodeVideoFromBuffer() throws Exception {
+        MediaCodec decoder = null;
+
+        try {
+            MediaCodecInfo codecInfo = selectCodec(MIME_TYPE, false);
+            if (codecInfo == null) {
+                Log.e(LOG_TAG, "Unable to find an appropriate codec for " + MIME_TYPE);
+                return;
+            }
+
+            Log.i(LOG_TAG, "Found codec: " + codecInfo.getName());
+
+            // JPEG decoder only support this colorFormat as Output
+            // COLOR_FormatCbYCrY a.k.a Organized as 16bit UYVY
+            int colorFormat = MediaCodecInfo.CodecCapabilities.COLOR_FormatCbYCrY;
+
+            MediaFormat format = MediaFormat.createVideoFormat(MIME_TYPE, mWidth, mHeight);
+
+            // Set some properties.  Failing to specify some of these can cause the MediaCodec
+            // configure() call to throw an unhelpful exception.
+            //format.setInteger(MediaFormat.KEY_COLOR_FORMAT, colorFormat);
+            //format.setInteger(MediaFormat.KEY_BIT_RATE, mBitRate);
+            //format.setInteger(MediaFormat.KEY_FRAME_RATE, FRAME_RATE);
+            //format.setInteger(MediaFormat.KEY_I_FRAME_INTERVAL, IFRAME_INTERVAL);
+            Log.i(LOG_TAG, "format: " + format);
+
+            // Create a MediaCodec for the desired codec, then configure it as an encoder with
+            // our desired properties.
+            decoder = MediaCodec.createByCodecName(codecInfo.getName());
+            Log.i(LOG_TAG, "created Codec " + codecInfo.getName());
+            decoder.configure(format, null, null, 0);
+            decoder.start();
+
+            doDecodeVideoFromBuffer(decoder, colorFormat);
+
+        } finally {
+
+            Log.i(LOG_TAG, "releasing decoder");
+            if (decoder != null) {
+                decoder.stop();
+                decoder.release();
+            }
+
+        }
+
+    }
+
     /**
      * Returns the first codec capable of encoding the specified MIME_TYPE
      */
-    private static MediaCodecInfo selectCodec(String mimeType) {
+    private static MediaCodecInfo selectCodec(String mimeType, boolean needEncoder) {
         int numCodec = MediaCodecList.getCodecCount();
         Log.i(LOG_TAG, "We got " + numCodec + " Codecs");
 
         for (int i = 0; i < numCodec; i++) {
             MediaCodecInfo codecInfo = MediaCodecList.getCodecInfoAt(i);
 
-            if (!codecInfo.isEncoder()) {
+            if (!codecInfo.isEncoder() && needEncoder) {
                 continue;
             }
 
@@ -159,7 +211,6 @@ public class EncoderDecoderTestForMJPEG extends AndroidTestCase {
     private void doEncodeVideoFromBuffer(MediaCodec encoder, int encoderColorFormat) throws IOException {
         Log.i(LOG_TAG, "In doEncodeVideoFromBuffer...");
 
-        final int TIME_OUT = 10000; //usec
         int generateIndex = 0;
 
         ByteBuffer[] encoderInputBuffers = encoder.getInputBuffers();
@@ -204,7 +255,7 @@ public class EncoderDecoderTestForMJPEG extends AndroidTestCase {
                         inputDone = true;
                     } else {
                         // Store input data into frameData
-                        generateFrame(generateIndex, encoderColorFormat, frameData, true);
+                        generateFrame(generateIndex, encoderColorFormat, frameData, true, true);
 
                         ByteBuffer inputBuf = encoderInputBuffers[inputBufIndex];
                         // Buf capacity check
@@ -276,6 +327,143 @@ public class EncoderDecoderTestForMJPEG extends AndroidTestCase {
         }
     }
 
+    /**
+     * Do the actual work for decoding frames from buffers of byte[].
+     * @param decoder
+     * @param decoderColorFormat
+     *
+     */
+    private void doDecodeVideoFromBuffer(MediaCodec decoder, int decoderColorFormat)
+            throws IOException {
+        Log.i(LOG_TAG, "In doDecodeVideoFromBuffer ...");
+
+        // As for decoder, the input file is video.jpeg the output file is video.YUY2
+        // which is oppsite with encoder
+        String INPUT = OUTPUT_FILE;
+        String OUTPUT = INPUT_FILE;
+
+        File input = new File(INPUT);
+
+        int generateIndex = 0;
+
+        ByteBuffer[] decoderInputBuffers = decoder.getInputBuffers();
+
+        MediaCodec.BufferInfo info = new MediaCodec.BufferInfo();
+
+        // The size of input picture
+        byte[] frameData = new byte[(int)input.length()];
+
+        // decoded File
+        FileOutputStream outputStream = new FileOutputStream(OUTPUT);
+
+
+        boolean inputDone = false;
+        boolean decodeDone = false;
+        boolean outputDone = false;
+
+        while (!outputDone) {
+            Log.i(LOG_TAG, "Loopping...");
+
+            // If we're not done submitting frames, generate a new one and submit it.  By
+            // doing this on every loop we're working to ensure that the encoder always has
+            // work to do.
+            //
+            // We don't really want a timeout here, but sometimes there's a delay opening
+            // the encoder device, so a short timeout can keep us from spinning hard.
+            if (!inputDone) {
+                int inputBufIndex = decoder.dequeueInputBuffer(TIME_OUT);
+                Log.i(LOG_TAG, "inputBufIndex = " + inputBufIndex);
+
+                if (inputBufIndex >= 0) {
+                    long ptsUsec = computePTS(generateIndex);
+
+                    if (generateIndex == NUM_FRAMES) {
+                        // We have enough frames, so send an empty one with EOS flag
+                        Log.d(LOG_TAG, "Queuing EOS in InputBuffer");
+                        decoder.queueInputBuffer(inputBufIndex, 0, 0, 0,
+                                MediaCodec.BUFFER_FLAG_END_OF_STREAM);
+                        inputDone = true;
+                    } else {
+                        // Store input data into frameData
+                        generateFrame(generateIndex, decoderColorFormat, frameData, true, false);
+
+                        ByteBuffer inputBuf = decoderInputBuffers[inputBufIndex];
+                        // Buf capacity check
+                        Log.i(LOG_TAG, "inputBuf.capacity() = " + inputBuf.capacity()
+                                + "frameData.length = " + frameData.length);
+                        assertTrue(inputBuf.capacity() >= frameData.length);
+                        inputBuf.clear();
+                        inputBuf.put(frameData);
+
+                        decoder.queueInputBuffer(inputBufIndex, 0, frameData.length, ptsUsec, 0);
+                        Log.i(LOG_TAG, "Buffer " + inputBufIndex + " of len = " + frameData.length + " submitted to decode");
+                    }
+
+                    generateIndex++;
+                } else {
+                    Log.w(LOG_TAG, "Input buffer not available");
+                }
+            }
+
+            // Check for output from the decoder.  We want to do this on every loop to avoid
+            // the possibility of stalling the pipeline.  We use a short timeout to avoid
+            // burning CPU if the decoder is hard at work but the next frame isn't quite ready.
+            //
+            // If we're decoding to a Surface, we'll get notified here as usual but the
+            // ByteBuffer references will be null.  The data is sent to Surface instead.
+            if (!decodeDone) {
+                Log.d(LOG_TAG, "dequeuing Output buffers...");
+                int decoderStatus = decoder.dequeueOutputBuffer(info, TIME_OUT);
+                if (decoderStatus == MediaCodec.INFO_TRY_AGAIN_LATER) {
+                    Log.i(LOG_TAG, "No output from decoder available, try again later.");
+                } else if (decoderStatus == MediaCodec.INFO_OUTPUT_BUFFERS_CHANGED) {
+                    Log.i(LOG_TAG, "INFO_OUTPUT_BUFFERS_CHANGED... NOT EXPECTED for and decoder");
+                } else if (decoderStatus == MediaCodec.INFO_OUTPUT_FORMAT_CHANGED) {
+                    Log.i(LOG_TAG, "INFO_OUTPUT_FORMAT_CHANGED... NOT EXPECTED for and decoder");
+                } else if (decoderStatus < 0) {
+                    Log.e(LOG_TAG, "Unexpected result!!!");
+                    fail("decoder in wrong Status");
+                } else { // it's the index of an output buffer that has been successfully decoded
+                    Log.i(LOG_TAG, "Decode success, decoderStatus =  " + decoderStatus + " info.size = " + info.size);
+
+                    ByteBuffer[] decoderOutputBuffers = decoder.getOutputBuffers();
+
+                    ByteBuffer decodedData = decoderOutputBuffers[decoderStatus];
+                    if (decodedData == null) {
+                        fail("decoderOuputBuffer " + decoderStatus + " was null");
+                    }
+
+                    Log.i(LOG_TAG, "outputStream = " + outputStream);
+
+                    decodedData.position(info.offset);
+                    decodedData.limit(info.offset + info.size);
+
+                    if (outputStream != null) {
+                        byte[] data = new byte[info.size];
+                        decodedData.get(data); //Store input buffer into data
+                        decodedData.position(info.offset);
+
+                        if (info.size > 0) {
+                            Log.i(LOG_TAG, "outputStream writing data! size = " + info.size);
+                            outputDone = true;
+                            outputStream.write(data);
+                        }
+                    }
+
+                    //FIXME: Check info.flags is not working ???
+                    //if (generateIndex >= NUM_FRAMES) {
+                    //    outputDone = true;
+                    //}
+
+                    decoder.releaseOutputBuffer(decoderStatus, false);
+                    decodeDone = true;
+
+                }
+            }
+        }
+
+    }
+
     private long computePTS(int generateIndex) {
         return 132 + generateIndex * 1000000 / FRAME_RATE;
     }
@@ -296,11 +484,22 @@ public class EncoderDecoderTestForMJPEG extends AndroidTestCase {
      * </pre>
      * We draw one of the eight rectangles and leave the rest set to the zero-fill color.
      */
-    private void generateFrame(int frameIndex, int colorFormat, byte[] frameData, boolean readFromDisk) {
-        Log.i(LOG_TAG, "Generating frame " + frameIndex);
+    private void generateFrame(int frameIndex, int colorFormat,
+                               byte[] frameData, boolean readFromDisk,
+                               boolean encoder) {
+        Log.i(LOG_TAG, "Generating frame " + frameIndex + "for " + (encoder ? "encoder" : "decoder"));
 
         FileInputStream fileInputStream = null;
-        File file = new File(INPUT_FILE);
+        File file = null;
+
+        // If it's decoder the OUTPUT_FILE name is as INPUT
+        if (encoder) {
+            file = new File(INPUT_FILE);
+        } else {
+            file = new File(OUTPUT_FILE);
+        }
+
+        Log.i(LOG_TAG, "file.length = " + file.length());
 
 
         if (readFromDisk) {
